630159a818377a58475199cf3c9e51b1e8164d64
Rudy Perez <rrudy90023@gmail.com>
2013-12-10 22:27:57 -0800

    Added names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,4 +4,8 @@ belongs_to :user
 
 has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 
+
+validates :image, presence: true
+validates :description, presence: true
+
 end
